1079ac080b98081a4781b1775d02842be35a39a6
levase <levase@mail.ru>
2020-06-14 12:10:23 +0400

    les1

--- homeworks/les1/task1.py
+++ homeworks/les1/task1.py
@@ -1,1 +1,17 @@
-"HOME WORK"
+a = 1
+b = 2.1
+c = 'Программа'
+d = 'работает'
+
+name = input('Ваше имя?\n')
+surname = input('Ваша фамилия?\n')
+
+while True:
+    age = input('Сколько Вам лет?\n')
+    if age.isdigit():
+        age = int(age)
+        break
+    print('Введите возраст числом')
+
+print(f'Вас зовут {name} {surname}, {2020-age} года рождения\n')
+print(c, d)
